c3e6bdc0bc0522c1d06f8511d873038afce64f86
민재준 <minjaejun291@gmail.com>
2024-01-11 19:15:44 +0900

    Routing

--- server.py
+++ server.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'random : <strong>' + str(random.random()) +'</strong>'
+    return 'random: <strong>' + str(random.random()) + '</strong>'
+
+@app.route('/create/')
+def create():
+    return 'Create'
+
+@app.route('/read/<id>/')
+def read(id):
+    print(id)
+    return 'Read : <strong>' + id +'</strong>'
 
 app.run()
